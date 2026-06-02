1b65b0b54764e14011fa885a2b858427348b48b0
abdominal07 <103875038+abdominal07@users.noreply.github.com>
2026-06-02 17:33:56 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,7 @@ import MintNFT from "../components/MintNFT";
 
 export default function Home() {
   return (
-    <main
-      style={{
-        maxWidth: "800px",
-        margin: "0 auto",
-        padding: "40px",
-        textAlign: "center"
-      }}
-    >
-      <h1>IOPN Genesis NFT</h1>
-
-      <p>
-        Mint your NFT on OPN Chain
-      </p>
-
+    <main>
       <MintNFT />
     </main>
   );
